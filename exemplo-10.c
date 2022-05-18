ae5693429e04e4f85d53fb18c6a16f5ccc774deb
arthurjosee <104663545+arthurjosee@users.noreply.github.com>
2022-05-17 22:10:32 -0300

    Add files via upload

---
+++ exemplo-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main() {
+	int x;
+	scanf ("%i", &x);
+	while (x==0){
+		printf ("Erro\n");
+		scanf ("%i", &x);
+		
+// n-- (decrementa de 1 em 1 a variavel n)
+// n++ (aumenta de 1 em 1 a variavel n)
+// n+=x "ex: n+=3" (aumenta x vezes a variavel n)
+
+	}
+	printf ("Agora sim");
+return 0;
+}
+
